1795ebee7463619af300f311b40ac8d556606d13
jawadyousoufi <javadyousefi21@gmail.com>
2022-01-30 15:40:32 +0000

    kep pair output

--- key-pair/output.tf
+++ key-pair/output.tf
@@ -1,7 +1,7 @@
 output "aws_region" {
     value       = data.aws_region.deployer.name
   }
-output "key_name" {
+output "key_pair_name" {
     value       = aws_key_pair.deployer.key_name
   }
 output "key_pair_id" {
